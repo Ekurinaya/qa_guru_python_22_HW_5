4b0188c0c09af8b18f642304094ce3ba73b15e3d
Ekaterina Kurinaya <ekaterinakurinaa@MacBook-Pro-2.local>
2025-10-05 14:53:17 +0300

    files added, test created

---
+++ tests/test_for_demoqa.py
@@ -0,0 +1,26 @@
+from selene import browser, be
+from pathlib import Path
+
+def test_for_demoqa():
+    browser.open('https://demoqa.com/automation-practice-form')
+    browser.element('#firstName').type('Ekaterina')
+    browser.element('#lastName').type('Kurinaya')
+    browser.element('#userEmail').type('cat@gmail.com')
+    browser.element('label[for="gender-radio-2"]').click()
+    browser.element('#userNumber').type('1234567890')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('.react-datepicker__year-select option[value="1995"]').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('.react-datepicker__month-select option[value="0"]').click()
+    browser.element('.react-datepicker__day--017').click()
+    browser.element('#subjectsInput').type('History').press_enter()
+    browser.element('#subjectsInput').type('Civics').press_enter()
+    browser.element('label[for="hobbies-checkbox-2"]').click()
+    browser.element('label[for="hobbies-checkbox-3"]').click()
+    browser.element('#uploadPicture').set_value(str(Path(__file__).parent / 'resources' / 'cat.png'))
+    browser.element('#currentAddress').type('Eftorius, 5')
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#react-select-4-input').type('Noida').press_enter()
+    browser.element('#submit').should(be.clickable).click()
